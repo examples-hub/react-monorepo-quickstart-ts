23c745ca2e166215c7cf62dae184204fcccef95b
uptonking <jinyaoo86@gmail.com>
2022-06-13 02:31:14 +0800

    refactor: remove enzyme from testing

--- packages/sample-react-components-ts/src/CheckboxWithLabel.test.tsx
+++ packages/sample-react-components-ts/src/CheckboxWithLabel.test.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
 import CheckboxWithLabel from './CheckboxWithLabel';
 
 test('CheckboxWithLabel changes the text after click', () => {
   // Render a checkbox with label in the document
-  const checkbox = shallow(<CheckboxWithLabel labelOn='On' labelOff='Off' />);
+  const checkbox = (<CheckboxWithLabel labelOn='On' labelOff='Off' />) as any;
 
   expect(checkbox.text()).toEqual('Off');
 
--- packages/sample-react-components/src/CheckboxWithLabel.test.js
+++ packages/sample-react-components/src/CheckboxWithLabel.test.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
 import CheckboxWithLabel from './CheckboxWithLabel';
 
 test('CheckboxWithLabel changes the text after click', () => {
   // Render a checkbox with label in the document
-  const checkbox = shallow(<CheckboxWithLabel labelOn='On' labelOff='Off' />);
+  // const checkbox = shallow(<CheckboxWithLabel labelOn='On' labelOff='Off' />);
+  const checkbox = <CheckboxWithLabel labelOn='On' labelOff='Off' />;
 
   expect(checkbox.text()).toEqual('Off');
 
